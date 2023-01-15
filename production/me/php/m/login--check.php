9c63a3af1138a9de5f4e2c1d7d8ed98af8cbdbaa
checkthesound <danil.dybko@gmail.com>
2023-01-16 01:37:11 +0300

    plans

---
+++ production/me/php/m/login--check.php
@@ -0,0 +1,18 @@
+<?php
+
+$email = $_POST['email'];
+$password = $_POST['passw'];
+
+// check len
+
+// to DB
+
+// get user at email
+
+// check len
+
+// get from founded user password
+
+// check my password and user password
+
+// set cookie
